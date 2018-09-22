a61c5de8dc16750815ba820ece3bec9971616db4
googiell <gluglawojciech@gmail.com>
2018-09-23 00:18:11 +0200

    Base react-router structure

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Movie = () => {
+  return (
+    <div>
+      <h2> Movie Title </h2>
+      <p> Movie Desc </p>
+    </div>
+  );
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import { AppHeader } from '../../components/AppHeader/AppHeader';
+import { Movie } from '../../components/Movie/Movie';
 
 class App extends Component {
+  componentDidMount(){};
   render() {
     return (
-      <div>
-        <header>
-          <h1>Welcome to React</h1>
-        </header>
-        <p >
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Link to='/'> Home </Link>
+          <Link to='/movie'> Movie </Link>
+          <Route exact path='/' component={AppHeader} />
+          <Route path='/movie' component={Movie} />
+        </div>
+      </Router>
     );
   }
 }
