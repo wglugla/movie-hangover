fce58add3ab9434b96e00073724cd25a8f380d74
googiell <gluglawojciech@gmail.com>
2018-09-25 20:06:19 +0200

    Active redux_devtools__extension

--- src/store.js
+++ src/store.js
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
+/* eslint-disable no-underscore-dangle */
 export const store = createStore(
   reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(ReduxThunk)
 );
+/* eslint-enable */
